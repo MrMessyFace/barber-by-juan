a22034cd515a71c280e2de9ea75100a494156dd2
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-11-15 15:30:17 -0600

    mobile css test

--- src/components/HoursInfo.js
+++ src/components/HoursInfo.js
@@ -3,13 +3,14 @@ import React from "react";
 const HoursInfo = () => {
   return (
     <div className="hours">
-      <p>Monday: 10:00am - 8:00pm</p>
-      <p>Tuesday: 10:00am - 8:00pm</p>
-      <p>Wednesday: 10:00am - 8:00pm</p>
-      <p>Thursday: 10:00am - 8:00pm</p>
-      <p>Friday: 10:00am - 8:00pm</p>
-      <p>Saturday: 9:30am - 7:00pm</p>
-      <p>Sunday: 11:00am - 3:00pm</p>
+      <p>MONDAY - FRIDAY</p>
+      <p>10:00am - 8:00pm</p>
+      <br></br>
+      <p>SATURDAY</p>
+      <p>9:30am - 7:00pm</p>
+      <br></br>
+      <p>SUNDAY</p>
+      <p>11:00am - 3:00pm</p>
     </div>
   );
 };
