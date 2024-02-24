df1197ac0ecc41b29393222c2c5c5eb82adb0b5c
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2024-02-24 16:08:07 -0600

    added photos to slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -4,9 +4,11 @@ import img02 from "../assets/images/slideshow-image-02.jpg";
 import img03 from "../assets/images/slideshow-image-03.jpg";
 import img04 from "../assets/images/slideshow-image-04.jpg";
 import img05 from "../assets/images/slideshow-image-05.jpg";
+import img06 from "../assets/images/slideshow-image-06.jpg";
+import img07 from "../assets/images/slideshow-image-07.jpg";
 
 const Slideshow = () => {
-  const images = [img01, img02, img03, img04, img05];
+  const images = [img01, img02, img03, img04, img05, img06, img07];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
