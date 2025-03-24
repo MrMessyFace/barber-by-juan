dfc1ecfad338bcc9d012932da9c22e3fb0ede01c
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2025-03-24 14:40:09 -0500

    added photos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,11 +15,38 @@ const Gallery = () => {
       <div className="gallery-caption">
         <div className="gallery-photo">
           <img
+            src={require("../assets/images/gallery-image-08.jpg")}
+            alt="barbershop"
+          />
+        </div>
+        <p className="invisible">Image 2</p>
+      </div>
+      <div className="gallery-caption">
+        <div className="gallery-photo">
+          <img
+            src={require("../assets/images/gallery-image-09.jpg")}
+            alt="barbershop"
+          />
+        </div>
+        <p className="invisible">Image 3</p>
+      </div>
+      <div className="gallery-caption">
+        <div className="gallery-photo">
+          <img
+            src={require("../assets/images/gallery-image-10.jpg")}
+            alt="barbershop"
+          />
+        </div>
+        <p className="invisible">Image 4</p>
+      </div>
+      <div className="gallery-caption">
+        <div className="gallery-photo">
+          <img
             src={require("../assets/images/gallery-image-02.jpg")}
             alt="haircut"
           />
         </div>
-        <p className="invisible">Image 2</p>
+        <p className="invisible">Image 5</p>
       </div>
       <div className="gallery-caption">
         <div className="gallery-photo">
@@ -28,7 +55,7 @@ const Gallery = () => {
             alt="haircut"
           />
         </div>
-        <p className="invisible">Image 3</p>
+        <p className="invisible">Image 6</p>
       </div>
       <div className="gallery-caption">
         <div className="gallery-photo">
@@ -37,7 +64,7 @@ const Gallery = () => {
             alt="haircut"
           />
         </div>
-        <p className="invisible">Image 4</p>
+        <p className="invisible">Image 7</p>
       </div>
       <div className="gallery-caption">
         <div className="gallery-photo">
@@ -46,7 +73,7 @@ const Gallery = () => {
             alt="haircut"
           />
         </div>
-        <p className="invisible">Image 5</p>
+        <p className="invisible">Image 8</p>
       </div>
       <div className="gallery-caption">
         <div className="gallery-photo">
@@ -66,6 +93,7 @@ const Gallery = () => {
         </div>
         <p>After a Perm</p>
       </div>
+      
     </div>
   );
 };
